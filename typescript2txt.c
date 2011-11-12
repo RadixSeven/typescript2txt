3b276ec9ef5c77b20b24a37e705ab0c92311ea55
Eric David Moyer <eric_moyer@yahoo.com>
2011-11-12 12:12:38 -0500

    Changed indentation to my preferred indents

--- typescript2txt.c
+++ typescript2txt.c
@@ -38,20 +38,20 @@ int32_t  buffer_size=0;
 int32_t  len;
 
 void addchar(int c) {
-	if(len >= buffer_size) {
-		if(buffer_size==0) {
-			buffer_size=128;
-		} else {
-			buffer_size*=2;
-		}
-		buffer=(char*)realloc((void *)buffer, buffer_size);
-		if (buffer==NULL) {
-			fprintf(stderr,"\nLINE TOO LONG FOR MEMORY!!!\n");
-			exit(1);
-		}
-	}
-	buffer[len]=c;
-	len++;
+  if(len >= buffer_size) {
+    if(buffer_size==0) {
+      buffer_size=128;
+    } else {
+      buffer_size*=2;
+    }
+    buffer=(char*)realloc((void *)buffer, buffer_size);
+    if (buffer==NULL) {
+      fprintf(stderr,"\nLINE TOO LONG FOR MEMORY!!!\n");
+      exit(1);
+    }
+  }
+  buffer[len]=c;
+  len++;
 }
 
 /* caret converts a charater M into a character '^M' */
@@ -59,68 +59,68 @@ int caret(int c) { return (c-'A'+1); }
 int decaret(int c) { return (c+'A'-1); }
 
 void handleESC(char c) {
-	int s[16];
-	int i=0;
-	int n=0;
-	s[i]=getchar();
-	if (isdigit(s[i])) { n=n*10+(s[i]-'0'); i++;  s[i]=getchar();}
-	if (isdigit(s[i])) { n=n*10+(s[i]-'0'); i++;  s[i]=getchar();}
-	if (isdigit(s[i])) { n=n*10+(s[i]-'0'); i++;  s[i]=getchar();}
-	if (isdigit(s[i])) { n=n*10+(s[i]-'0'); i++;  s[i]=getchar();}
-	if (c=='[') {
-		if (s[0]=='0' && s[1]=='1' && s[2]==';') {
-			//  ^[[01;34m == <colour='cyan'>
-			//  ^[[01;31m == <colour='red'>
-			//  ^[[00m == </colour>
-		 i++;  s[i]=getchar();
-		 i++;  s[i]=getchar();
-		 i++;  s[i]=getchar();
-		}
-		if (s[i]=='P'){
-			len-=n;
-			if(len<0) {
-				len=0;
-			}
-		}
-		if (s[i]=='@'){
-			addchar(' '); //should add n instances of ' '
-		}
-	} else if (c==']') { // Just strip out all "set icon" and "set title" commands
-		while(getchar()>caret('G'));
-	}
-	if (s[0]=='A') {
-		len=2;
-	}
-}
-			
-int main () {
-	int c,d;
-	int lastM=0; // last character was an '^M'
-	int lastESC=0; // last character was an '^[' 
+  int s[16];
+  int i=0;
+  int n=0;
+  s[i]=getchar();
+  if (isdigit(s[i])) { n=n*10+(s[i]-'0'); i++;  s[i]=getchar();}
+  if (isdigit(s[i])) { n=n*10+(s[i]-'0'); i++;  s[i]=getchar();}
+  if (isdigit(s[i])) { n=n*10+(s[i]-'0'); i++;  s[i]=getchar();}
+  if (isdigit(s[i])) { n=n*10+(s[i]-'0'); i++;  s[i]=getchar();}
+  if (c=='[') {
+    if (s[0]=='0' && s[1]=='1' && s[2]==';') {
+      //  ^[[01;34m == <colour='cyan'>
+      //  ^[[01;31m == <colour='red'>
+      //  ^[[00m == </colour>
+      i++;  s[i]=getchar();
+      i++;  s[i]=getchar();
+      i++;  s[i]=getchar();
+    }
+    if (s[i]=='P'){
+      len-=n;
+      if(len<0) {
 	len=0;
- 	while ((c=getchar())>=0) {
-		d=decaret(c);
-		if (lastM && d!='J') {
-			len=0;
-		}
-		lastM=0;
-		if (c<32) {
-			lastESC=0;
-			switch(d) {
-				case 'H' : len--; break;
-			        case 'J' : addchar(0); puts(buffer); len=0; break;
-				case 'M' : lastM=1; break;
-				case 'G' : break;
-				case '[' : lastESC=1; break;
-				default  : addchar('^'); addchar(d); addchar(c);
-			}
-		} else {
-			if (lastESC && (c=='[' || c==']')) {
-				handleESC(c);
-			} else {
-				addchar(c);
-			}
-		}
-	}
-	return 0;
+      }
+    }
+    if (s[i]=='@'){
+      addchar(' '); //should add n instances of ' '
+    }
+  } else if (c==']') { // Just strip out all "set icon" and "set title" commands
+    while(getchar()>caret('G'));
+  }
+  if (s[0]=='A') {
+    len=2;
+  }
+}
+
+int main () {
+  int c,d;
+  int lastM=0; // last character was an '^M'
+  int lastESC=0; // last character was an '^[' 
+  len=0;
+  while ((c=getchar())>=0) {
+    d=decaret(c);
+    if (lastM && d!='J') {
+      len=0;
+    }
+    lastM=0;
+    if (c<32) {
+      lastESC=0;
+      switch(d) {
+      case 'H' : len--; break;
+      case 'J' : addchar(0); puts(buffer); len=0; break;
+      case 'M' : lastM=1; break;
+      case 'G' : break;
+      case '[' : lastESC=1; break;
+      default  : addchar('^'); addchar(d); addchar(c);
+      }
+    } else {
+      if (lastESC && (c=='[' || c==']')) {
+	handleESC(c);
+      } else {
+	addchar(c);
+      }
+    }
+  }
+  return 0;
 }
